23220bdc20e895aa4b6cd6308597fcb8019f2e4d
JeffNania <60474400+JeffNania@users.noreply.github.com>
2020-11-30 22:31:26 -0500

    Add files via upload

--- JeffNania_Project.py
+++ JeffNania_Project.py
@@ -21,8 +21,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
+from collections import Counter
 from sklearn import metrics
 from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import (KFold, ShuffleSplit, StratifiedKFold, StratifiedShuffleSplit)
 from sklearn.ensemble import RandomForestClassifier
@@ -47,6 +50,7 @@ Diabetes_Predictors = ['Age', 'Pregnancies', 'Glucose', 'BloodPressure', 'SkinTh
 
 print(Diabetes.describe(include='all'))
 
+Diabetes.head()
 #%% Histograms
 
 Diabetes['Age'].hist()
@@ -85,40 +89,127 @@ Diabetes['BMI'].corr(Diabetes['Outcome'])
 
 Diabetes['DiabetesPedigreeFunction'].corr(Diabetes['Outcome'])
 
-#%% Find Averages, Min, and Max of all factors
+#%% Find Averages, Min, and Max of all factors as well as count
+
+# mean, min, max
 
 Diabetes_Predictors_means = Diabetes.groupby('Outcome').mean()
+
 Diabetes_Predictors_mins = Diabetes.groupby('Outcome').min()
+
 Diabetes_Predictors_maxes = Diabetes.groupby('Outcome').max()
+
+# Counts
+
+Diabetes_Predictors_count = Diabetes.groupby('Outcome').count()
+
+Diabetes_Predictors_count = Diabetes.groupby('Outcome').agg(['count','mean'])
+
+Diabetes_Positive_count = Diabetes.groupby(Outcome==1).count() ## DOES NOT WORK
+
 
 #%% Create labels for binary classification #FIX THIS
 
-Diabetes['label'] = np.where(Diabetes['Outcome'] == 'Outcome', 'good', 'bad')
+Diabetes.Outcome.replace({0:'No Diabetes',1:'Positive for Diabetes'},inplace=True)
 
-#%%
-
-
-#%% Simple plots #FIX THIS
-
-# Create a plot that
-
-Diabetes_Plot=Diabetes.groupby(['Age', 'Glucose'])['Outcome'].sum().unstack().plot(logy=False, figsize=(100,10))
-Diabetes_Plot.set(xlabel='Diabetes_Predictors', ylabel='Diabetes')
-Diabetes_Plot.get_figure()
+print(Diabetes.Outcome.value_counts())
 
 #%% Segment Data #FIX THIS
 
-train, test = train_test_split(netattacks, test_size=0.25)
+train, test = train_test_split(Diabetes, test_size=0.25)
+
 print("Rows in train:", len(train))
+
 print("Rows in test:", len(test))
 
 #%% Train Model #FIX THIS
 
+#Decision Tree
+
 # define new tree
 dt = tree.DecisionTreeClassifier()
-# train the model using the 9th and 10th columns
-# The value we are trying to predict is 'label'
-dt.fit(train.iloc[:, 9:10], train['label'])
+
+# train the model using the 2nd and 6th columns (Glucose and BMI)
+# The value we are trying to predict is 'Outcome'
+
+dt.fit(train.iloc[:, 2:6], train['Outcome'])
+
+# Tree depth
+print(dt.get_depth())
+
+
+#%% Predict Labels for Test Data # FIX THIS
+
+predicted = dt.predict(test.iloc[:, 2:6])
+
+print(predicted[:5]) # show first five predictions
+
+# count test data
+test_labels_stats = Counter(test['Outcome'])
+
+print("Labels in the test data:", test_labels_stats)
+
+# count predicted
+predicted_labels_stats = Counter(predicted)
+
+print("Labels in the predictions:", predicted_labels_stats)
+#%% Model Stats # FIX THIS
+
+# Classification report
+
+print(metrics.classification_report(test['Outcome'], Diabetes_Predictors, digits=5))
+
+#Confusion Matrix
+
+metrics.confusion_matrix(y_true=test['label'], y_pred=predicted, labels=['good', 'bad'])
+
+metrics.plot_confusion_matrix(dt, test.iloc[:,2:6], test['label'], labels=['good', 'bad'])
+
+#Accuracy
+
+# compute baseline accuracy (predict all bad)
+
+baseline = test_labels_stats['bad'] / (test_labels_stats['good'] + test_labels_stats['bad'])
+
+print("Baseline accuracy is:", baseline)
+
+# compute the observed accuracy
+
+acc = metrics.accuracy_score(test['label'], predicted)
+
+print("Observed accuracy is:", acc)
+
+result = metrics.classification_report(test['label'], predicted, digits=4)
+
+print(result)
+#%% Random Forest # Fix This
+
+rf = ensemble.RandomForestClassifier()
+
+rf.fit(train[pred_vars], train['DEATH_EVENT'])
+
+#%% Naive Bayes # Fix This
+
+nb = GaussianNB()
+
+nb.fit(train[pred_vars], train['DEATH_EVENT'])
+
+#%% Logistic Regression # Fix This
+
+lr = LogisticRegression()
+
+lr.fit(train[pred_vars], train['DEATH_EVENT'])
+
+#%% Evaluation
+
+
+
+
+#%% View The Results
+
+
+
+
 
 #%% Questions
 
@@ -127,7 +218,7 @@ dt.fit(train.iloc[:, 9:10], train['label'])
 Glucose has a score of roughly 0.47 and BMI has a score of roughly 0.29"""
 
 
-# Which models seem to perform the best for this prediction?
+# 2) Which models seem to perform the best for this prediction?
 
 
 # Do the models perform best when considering all the factors or just some of the factors?
